2b25e77db2d9427ba63288623f9fa48a8e3e2c50
sanjin1110 <thapasanjin00@gmail.com>
2022-07-31 14:53:46 +0545

    Week6 Solution

--- Week6.java
+++ Week6.java
@@ -4,31 +4,31 @@ import java.util.Map;
 import java.util.Set;
 
 class Week6 {
-    public boolean isSolvable(String[] words, String result) {
+    public boolean canSolve(String[] words, String output) {
         Map<Character, Integer> letterDigitMap = new HashMap<Character, Integer>();
         Set<Character> leadingSet = new HashSet<Character>();
-        int resultLength = result.length();
+        int resultLength = output.length();
         for (String word : words) {
             if (word.length() > resultLength)
                 return false;
             if (word.length() > 1)
                 leadingSet.add(word.charAt(0));
         }
-        if (result.length() > 1)
-            leadingSet.add(result.charAt(0));
+        if (output.length() > 1)
+            leadingSet.add(output.charAt(0));
         boolean[] used = new boolean[10];
         int[] carry = new int[resultLength + 1];
-        return depthFirstSearch(words, result, letterDigitMap, leadingSet, used, carry, 0, 0);
+        return DFS(words, output, letterDigitMap, leadingSet, used, carry, 0, 0);
     }
 
-    public boolean depthFirstSearch(String[] words, String result, Map<Character, Integer> letterDigitMap, Set<Character> leadingSet, boolean[] used, int[] carry, int position, int wordIndex) {
-        if (position == result.length())
+    public boolean DFS(String[] words, String output, Map<Character, Integer> letterDigitMap, Set<Character> leadingSet, boolean[] used, int[] carry, int position, int wordIndex) {
+        if (position == output.length())
             return carry[position] == 0;
         else if (wordIndex < words.length) {
             String word = words[wordIndex];
             int wordLength = word.length();
             if (wordLength <= position || letterDigitMap.containsKey(word.charAt(wordLength - position - 1)))
-                return depthFirstSearch(words, result, letterDigitMap, leadingSet, used, carry, position, wordIndex + 1);
+                return DFS(words, output, letterDigitMap, leadingSet, used, carry, position, wordIndex + 1);
             else {
                 char letter = word.charAt(wordLength - position - 1);
                 int start = leadingSet.contains(letter) ? 1 : 0;
@@ -36,7 +36,7 @@ class Week6 {
                     if (!used[i]) {
                         used[i] = true;
                         letterDigitMap.put(letter, i);
-                        boolean next = depthFirstSearch(words, result, letterDigitMap, leadingSet, used, carry, position, wordIndex + 1);
+                        boolean next = DFS(words, output, letterDigitMap, leadingSet, used, carry, position, wordIndex + 1);
                         used[i] = false;
                         letterDigitMap.remove(letter);
                         if (next)
@@ -55,13 +55,13 @@ class Week6 {
             }
             carry[position + 1] = remain / 10;
             remain %= 10;
-            char letter = result.charAt(result.length() - position - 1);
+            char letter = output.charAt(output.length() - position - 1);
             if (letterDigitMap.containsKey(letter) && letterDigitMap.get(letter) == remain)
-                return depthFirstSearch(words, result, letterDigitMap, leadingSet, used, carry, position + 1, 0);
+                return DFS(words, output, letterDigitMap, leadingSet, used, carry, position + 1, 0);
             else if (!letterDigitMap.containsKey(letter) && !used[remain] && !(leadingSet.contains(letter) && remain == 0)) {
                 used[remain] = true;
                 letterDigitMap.put(letter, remain);
-                boolean next = depthFirstSearch(words, result, letterDigitMap, leadingSet, used, carry, position + 1, 0);
+                boolean next = DFS(words, output, letterDigitMap, leadingSet, used, carry, position + 1, 0);
                 used[remain] = false;
                 letterDigitMap.remove(letter);
                 return next;
@@ -73,6 +73,6 @@ class Week6 {
     	String word[] = new String[] {"THIS","IS","TOO"};
     	String result = "FUNNY";
     	Week6 obj = new Week6();
-    	System.out.println(obj.isSolvable(word, result));
+    	System.out.println(obj.canSolve(word, result));
     }
 }
