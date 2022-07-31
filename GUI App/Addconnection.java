d716687632118bb4f397eb860fe043141396578f
sanjin1110 <thapasanjin00@gmail.com>
2022-07-31 12:46:54 +0545

    pipe connection added

---
+++ GUI App/Addconnection.java
@@ -0,0 +1,30 @@
+
+package GuiApp;
+
+public class Addconnection extends javax.swing.JFrame {
+
+    public Addconnection() {
+        initComponents();
+    }
+
+
+    private void initComponents() {
+
+        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
+
+        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
+        getContentPane().setLayout(layout);
+        layout.setHorizontalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 400, Short.MAX_VALUE)
+        );
+        layout.setVerticalGroup(
+            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
+            .addGap(0, 300, Short.MAX_VALUE)
+        );
+
+        pack();
+    }
+
+
+}
